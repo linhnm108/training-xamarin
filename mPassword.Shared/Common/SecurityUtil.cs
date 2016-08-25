ace9ac0e2d9f1a9fbb04be67cb9455fd67c29301
linhnm108 <linhnm108@gmail.com>
2016-08-25 09:32:27 +0700

    validation and security utils

---
+++ mPassword.Shared/Common/SecurityUtil.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace mPassword.Shared
+{
+	public static class SecurityUtil
+	{
+		public static string SALT = "@B9-XamarinTraining";
+
+		public static string HashSHA256(string password)
+		{
+			var hashedPwd = new StringBuilder();
+
+			using (SHA256 hash = SHA256.Create())
+			{
+				Encoding enc = Encoding.UTF8;
+				var result = hash.ComputeHash(enc.GetBytes(password));
+
+				foreach (byte b in result)
+				{
+					hashedPwd.Append(b.ToString("x2"));
+				}
+			}
+			return hashedPwd.ToString();
+		}
+
+		public static string Encrypt(string clearText)
+		{
+			byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+			var pdb = new PasswordDeriveBytes(SALT, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+			byte[] encryptedData = Encrypt(clearBytes, pdb.GetBytes(32), pdb.GetBytes(16));
+			return Convert.ToBase64String(encryptedData);
+		}
+
+		public static string Decrypt(string cipherText)
+		{
+			byte[] cipherBytes = Convert.FromBase64String(cipherText);
+			var pdb = new PasswordDeriveBytes(SALT, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+			byte[] decryptedData = Decrypt(cipherBytes, pdb.GetBytes(32), pdb.GetBytes(16));
+			return Encoding.Unicode.GetString(decryptedData);
+		}
+
+		static byte[] Encrypt(byte[] clearText, byte[] Key, byte[] IV)
+		{
+			var ms = new MemoryStream();
+			Rijndael alg = Rijndael.Create();
+			alg.Key = Key;
+			alg.IV = IV;
+			var cs = new CryptoStream(ms, alg.CreateEncryptor(), CryptoStreamMode.Write);
+			cs.Write(clearText, 0, clearText.Length);
+			cs.Close();
+			byte[] encryptedData = ms.ToArray();
+			return encryptedData;
+		}
+
+		static byte[] Decrypt(byte[] cipherData, byte[] Key, byte[] IV)
+		{
+			var ms = new MemoryStream();
+			Rijndael alg = Rijndael.Create();
+			alg.Key = Key;
+			alg.IV = IV;
+			var cs = new CryptoStream(ms, alg.CreateDecryptor(), CryptoStreamMode.Write);
+			cs.Write(cipherData, 0, cipherData.Length);
+			cs.Close();
+			byte[] decryptedData = ms.ToArray();
+			return decryptedData;
+		}
+	}
+}
---
+++ mPassword.Shared/Common/ValicationUtil.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace mPassword.Shared
+{
+	public static class ValicationUtil
+	{
+		public static bool IsOverMaxLength(string strNeedCheck, int maxLength)
+		{
+			if (strNeedCheck.Length > maxLength)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public static bool IsValidPassword(string password)
+		{
+			var regex = new Regex(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$");
+			var match = regex.Match(password);
+			if (match.Success)
+			{
+				return true;
+			}
+			return false;
+		}
+
+	}
+}
