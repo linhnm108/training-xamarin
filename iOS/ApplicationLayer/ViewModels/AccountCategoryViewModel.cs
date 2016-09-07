c27c98e5d855a8c3c7ebb61341e63049b519c725
linhnm108 <linhnm108@gmail.com>
2016-09-07 07:54:53 +0700

    apply expandable table view

---
+++ iOS/ApplicationLayer/ViewModels/AccountCategoryViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+namespace mPassword.iOS
+{
+	public class AccountCategoryModel
+	{
+		public string categoryName;
+		public List<AccountViewModel> accounts;
+		public bool collapsed;
+		public int quantity;
+
+		public AccountCategoryModel(IList<BankAccount> accounts, bool collapsed)
+		{
+			categoryName = "Bank Accounts";
+			quantity = accounts.Count;
+			this.collapsed = collapsed;
+			this.accounts = new List<AccountViewModel>();
+
+			foreach (BankAccount bankAcc in accounts)
+			{
+				this.accounts.Add(new AccountViewModel(bankAcc));
+			}
+		}
+
+		public AccountCategoryModel(IList<ComputerAccount> accounts, bool collapsed)
+		{
+			categoryName = "Computer Accounts";
+			quantity = accounts.Count;
+			this.collapsed = collapsed;
+			this.accounts = new List<AccountViewModel>();
+
+			foreach (ComputerAccount computerAcc in accounts)
+			{
+				this.accounts.Add(new AccountViewModel(computerAcc));
+			}
+		}
+
+		public AccountCategoryModel(IList<EmailAccount> accounts, bool collapsed)
+		{
+			categoryName = "Email Accounts";
+			quantity = accounts.Count;
+			this.collapsed = collapsed;
+
+			this.accounts = new List<AccountViewModel>();
+
+			foreach (EmailAccount emailAcc in accounts)
+			{
+				this.accounts.Add(new AccountViewModel(emailAcc));
+			}
+		}
+
+		public AccountCategoryModel(IList<WebAccount> accounts, bool collapsed)
+		{
+			categoryName = "Web Accounts";
+			quantity = accounts.Count;
+			this.collapsed = collapsed;
+
+			this.accounts = new List<AccountViewModel>();
+
+			foreach (WebAccount webAcc in accounts)
+			{
+				this.accounts.Add(new AccountViewModel(webAcc));
+			}
+		}
+	}
+}
---
+++ iOS/ApplicationLayer/ViewModels/AccountViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+
+namespace mPassword.iOS
+{
+	public class AccountViewModel
+	{
+		public int accountId;
+		public string accountName;
+		public string accountType;
+		public bool isExpiredWarning;
+
+		public AccountViewModel(BankAccount bankAccount)
+		{
+			accountId = bankAccount.ID;
+			accountName = bankAccount.Name;
+			accountType = "BankAcc";
+
+			DateTime updatedDate = DateTime.ParseExact(bankAccount.UpdatedDate, "MM/dd/yyyy", CultureInfo.InvariantCulture);
+			DateTime expiredDate = updatedDate.AddDays(bankAccount.PasswordDuration);
+
+			long diffDays = (expiredDate - DateTime.Today).Ticks;
+
+			if (diffDays > 5)
+			{
+				isExpiredWarning = false;
+			}
+			else
+			{
+				isExpiredWarning = true;	
+			}
+		}
+
+		public AccountViewModel(ComputerAccount computerAccount)
+		{
+			accountId = computerAccount.ID;
+			accountName = computerAccount.Name;
+			accountType = "ComputerAcc";
+
+			DateTime updatedDate = DateTime.ParseExact(computerAccount.UpdatedDate, "MM/dd/yyyy", CultureInfo.InvariantCulture);
+			DateTime expiredDate = updatedDate.AddDays(computerAccount.PasswordDuration);
+
+			long diffDays = (expiredDate - DateTime.Today).Ticks;
+
+			if (diffDays > 5)
+			{
+				isExpiredWarning = false;
+			}
+			else
+			{
+				isExpiredWarning = true;
+			}
+		}
+
+		public AccountViewModel(WebAccount webAccount)
+		{
+			accountId = webAccount.ID;
+			accountName = webAccount.Name;
+			accountType = "WebAcc";
+			isExpiredWarning = false;
+		}
+
+		public AccountViewModel(EmailAccount emailAccount)
+		{
+			accountId = emailAccount.ID;
+			accountName = emailAccount.Name;
+			accountType = "EmailAcc";
+			isExpiredWarning = false;
+		}
+	}
+}
